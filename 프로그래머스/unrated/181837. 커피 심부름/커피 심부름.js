30fd5f98a5ffea0376f5422975fe3aa3391d8170
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2023-10-25 12:47:58 +0900

    [level unrated] Title: 커피 심부름, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -1,8 +1,5 @@
 function solution(order) {
     let money = 0;
-    const lattes = [
-        "icecafelatte", "cafelatteice", "hotcafelatte", "cafelattehot", "cafelatte"
-    ]
     order.forEach((c) => {
         c.includes('latte') ? money += 5000 : money += 4500;
     })
