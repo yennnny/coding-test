29946b398f80ad5af64152c66ea7281c346a278b
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-01-02 18:27:09 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,10 @@
+function solution(a, b, c) {
+    if(a !== b && a !== c && b !== c){
+        return a + b + c
+    } else if( a === b && a === c && b === c ){
+        return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3))
+    } else{
+         return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2))
+        
+    }
+}
