4c31b392c6c613633ce2c355697013eb6a77c0db
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-04-05 11:57:52 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,3 +1,7 @@
 function solution(my_string, k) {
-    return new Array(k).fill(my_string).join("")
+    // 내가 푼 방법
+    // return new Array(k).fill(my_string).join("");
+    
+    // repeat 사용하면 더 쉽다!
+    return my_string.repeat(k);
 }
