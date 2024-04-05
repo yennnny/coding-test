978a069f0e41278e76377e789f8e52232c829138
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-04-05 11:49:22 +0900

    [level 0] Title: 카운트 업, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,4 +1,16 @@
+// 처음 문제 풀이법
+// function solution(start_num, end_num) {
+//     let answer = [];
+//     let plusNum = start_num
+//     while(plusNum < end_num){
+//         answer.push(plusNum);
+//         plusNum += 1
+//     }
+//     answer.push(end_num)
+//     return answer;
+// }
+
+// Array.from 사용해 다시 풀기
 function solution(start, end) {
-    // Array.from 사용해 문제 풀기
     return Array.from({length : end - start + 1}, (_, i)=> start + i)
 }
