fa6f32e05e98d4f411e17d3b01d38180f83f5902
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-04-09 17:17:15 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -3,8 +3,6 @@ function solution(a, b) {
     const oddB = b % 2 !== 0;
     
     if(oddA && oddB) return a**2 + b** 2
-    
     if(oddA || oddB) return 2 * (a + b)
-
-    if(!oddA && !oddB) return Math.abs(a - b);
+    if (!oddA && !oddB) return Math.abs(a - b);
 }
