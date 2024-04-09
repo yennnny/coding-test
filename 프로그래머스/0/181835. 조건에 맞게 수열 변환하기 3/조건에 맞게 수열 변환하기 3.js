d23e434caed48e038ba000482bf62d89483a2ce8
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-04-09 17:06:01 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 2.77 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,14 +1,17 @@
 function solution(arr, k) {
-    let answer = [];
+//     let answer = [];
     
-    if(k % 2 === 0){
-        arr.map((it)=>{
-            answer.push(it + k)
-        })
-    } else{
-      arr.map((it)=>{
-            answer.push(it * k)
-        })  
-    }
-    return answer;
+//     if(k % 2 === 0){
+//         arr.map((it)=>{
+//             answer.push(it + k)
+//         })
+//     } else{
+//       arr.map((it)=>{
+//             answer.push(it * k)
+//         })  
+//     }
+//     return answer;
+    
+    // 또다른 풀이법
+    return k % 2 !== 0 ? arr.map(it => it * k) : arr.map(it => it + k);
 }
