ea4f1d8c84221073e312ecc63c30904e6f41b6cd
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-04-16 10:23:26 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,11 +1,20 @@
 function solution(s){
-    let pGroup = 0;
-    let yGroup = 0;
+    // 처음에 푼 문제풀이
+//     let pGroup = 0;
+//     let yGroup = 0;
     
-    for(let i of s){
-        i = i.toLowerCase();
-        if(i === 'p') pGroup += 1;
-        if(i === 'y') yGroup += 1;
-    }
-    return pGroup === yGroup ? true : false;
+//     for(let i of s){
+//         i = i.toLowerCase();
+//         if(i === 'p') pGroup += 1;
+//         if(i === 'y') yGroup += 1;
+//     }
+//     return pGroup === yGroup ? true : false;
+    
+    // reduce를 이용한 풀이
+    return [...s.toLowerCase()].reduce((acc, cur)=> {
+        if(cur === 'p') acc += 1;
+        if(cur === 'y') acc -= 1;
+        return acc;
+    }, 0) ? false : true;
+    
 }
