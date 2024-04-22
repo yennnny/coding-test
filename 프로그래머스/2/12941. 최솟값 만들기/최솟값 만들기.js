eb00ad06052cd81b0c4258b67580a03675fce92b
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-04-22 22:48:51 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.68 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,11 +1,18 @@
 function solution(A,B){
-    const aGroup = [...A].sort((a, b) => a - b);
-    const bGroup = [...B].sort((a, b) => b - a);
+// 처음 푼 방식
+    // const aGroup = [...A].sort((a, b) => a - b);
+    // const bGroup = [...B].sort((a, b) => b - a);
+    // let result = 0;
+
+    // for(let i = 0; i < aGroup.length; i += 1){
+    //     result += (aGroup[i] * bGroup[i]);     
+    // }
+    // return result;
     
-    let result = 0;
-    
-    for(let i = 0; i < aGroup.length; i += 1){
-        result += (aGroup[i] * bGroup[i]);     
-    }
-    return result;
+    // reduce를 활용한 방식
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    return A.reduce((acc, cur, idx) => {
+        return acc + cur * B[idx];
+    }, 0);
 }
