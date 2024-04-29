ff4ff7acbf27e1fe0307ac19e8e85b9da9883964
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-04-29 09:36:06 +0900

    [level 0] Title: 모음 제거, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,8 +1,4 @@
 function solution(my_string) {
-    let answer = '';
     const 모음 = ["a", "e", "i", "o", "u"];
-    for (let i = 0; i < my_string.length; i++) {
-    if (!모음.includes(my_string[i])) answer += my_string[i];
-  }
-  return answer;
+    return [...my_string].filter(x => !모음.includes(x)).join("");
 }
