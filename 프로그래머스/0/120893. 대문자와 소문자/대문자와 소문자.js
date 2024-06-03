9068c26617e9129b52bd80135c9a5105d3e8a1a0
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-06-04 05:32:14 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,6 @@
+function solution(my_string) {
+    return [...my_string].map(v => v === v.toUpperCase() 
+                             ? v.toLowerCase() 
+                             : v.toUpperCase() 
+                            ).join("")
+}
