c5f74a9573ff95b009d94dd2e07a5932c743deb6
Yeeun Jeong <84173008+yennnny@users.noreply.github.com>
2024-07-31 15:10:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,7 +1,7 @@
 function solution(s) {
     const numbers = ['zero','one','two','three','four','five','six','seven','eight','nine'];
     Number(numbers.forEach((n, idx) => s = s.replaceAll(n, idx)))
-    return Number(s)
+    return +s
     
 
 }
